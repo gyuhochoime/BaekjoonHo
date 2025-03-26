200a264b509b8c300f5386b2715845b368a0ae1a
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2025-03-26 12:53:30 +0900

    [Bronze III] Title: 연세여 사랑한다, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.py
@@ -0,0 +1,38 @@
+import sys
+n = sys.stdin.readline().rstrip()
+if n == "I":
+    print(84)
+elif n == "H" or n == "J":
+    print(85)
+elif n == "G" or n == "K":
+    print(86)
+elif n == "F" or n == "L":
+    print(87)
+elif n == "E" or n == "M":
+    print(88)
+elif n == "D" or n == "N":
+    print(89)
+elif n == "C" or n == "O":
+    print(90)
+elif n == "B" or n == "P":
+    print(91)
+elif n == "A" or n == "Q":
+    print(92)
+elif n == "R":
+    print(93)
+elif n == "S":
+    print(94)
+elif n == "T":
+    print(95)
+elif n == "U":
+    print(96)
+elif n == "V":
+    print(97)
+elif n == "W":
+    print(98)
+elif n == "X":
+    print(99)
+elif n == "Y":
+    print(100)
+elif n == "Z":
+    print(101)
