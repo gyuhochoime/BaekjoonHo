2b6c0b858aaa8b9e3491c846c2657735358e37f1
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2025-04-01 11:06:11 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.py
@@ -0,0 +1,14 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+m = "@" * n
+k = "@" * 5 * n
+for i in range(n):
+    print(m)
+for i in range(n):
+    print(m)
+for i in range(n):
+    print(m)
+for i in range(n):
+    print(m)
+for i in range(n):
+    print(k)
