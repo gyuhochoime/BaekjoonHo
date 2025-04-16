cc13e6969b5a75290da5f76c0129b0f0cde084a7
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2025-04-16 12:32:46 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,7 +1,8 @@
 import sys
 n = int(sys.stdin.readline().rstrip())
 m = list(sys.stdin.readline().rstrip())
+mod = 1234567891
 tot = 0
 for i in range(n):
     tot += (ord(m[i])-96) * 31 ** i
-print(tot)
+print(tot % mod)
