63e3e102f9c9b39a9139402b15355834482f302b
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2025-06-22 23:59:33 +0900

    [Bronze V] Title: 크냐?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -0,0 +1,9 @@
+import sys
+while True:
+    n, m = map(int, sys.stdin.readline().split())
+    if n == 0 and m == 0:
+        break
+    if n > m:
+        print("Yes")
+    else:
+        print("No")
