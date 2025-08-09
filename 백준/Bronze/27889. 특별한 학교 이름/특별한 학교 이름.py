75276401c583e32a39c4e60689560dd7997c477e
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2025-08-09 16:44:08 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+import sys
+n = sys.stdin.readline().rstrip()
+if n == "NLCS":
+    print("North London Collegiate School")
+elif n == "BHA":
+    print("Branksome Hall Asia")
+elif n == "KIS":
+    print("Korea International School")
+elif n == "SJA":
+    print("St. Johnsbury Academy")
