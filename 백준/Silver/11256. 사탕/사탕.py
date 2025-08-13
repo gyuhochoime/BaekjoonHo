febad67e4e7987b074a8b957faedd6d1e8e17391
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2025-08-13 21:04:02 +0900

    [Silver V] Title: 사탕, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11256. 사탕/사탕.py
+++ 백준/Silver/11256. 사탕/사탕.py
@@ -1,17 +1,16 @@
 import sys
 T = int(sys.stdin.readline().rstrip())
-for i in range(T):
-    j, n = map(int, sys.stdin.readline().rstrip().split())
+for _ in range(T):
     arr = []
     tot = 0
     cnt = 0
-    for i in range(n):
-        r, c = map(int, sys.stdin.readline().rstrip().split())
-        arr.append(r * c)
+    n, m = map(int, sys.stdin.readline().rstrip().split())
+    for _ in range(m):
+        a, b = map(int, sys.stdin.readline().rstrip().split())
+        arr.append(a * b)
     arr.sort(reverse = True)
     for i in arr:
-        tot += i
-        cnt += 1
-        if tot >= j:
-            break
+        if tot < n:
+            tot += i
+            cnt += 1
     print(cnt)
