949e7f83a58a32ffb2eeeaf5dd0b8c82da4c7948
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2025-10-16 22:28:52 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+import sys
+n = sys.stdin.readline().rstrip()
+if n == "SONGDO":
+    print("HIGHSCHOOL")
+elif n == "CODE":
+    print("MASTER")
+elif n == "2023":
+    print("0611")
+elif n == "ALGORITHM":
+    print("CONTEST")
