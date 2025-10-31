a193b256da73761cabf215fc2e1378d07bb6da57
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2025-10-31 19:49:08 +0900

    [Bronze IV] Title: 공백 없는 A+B, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/15873. 공백 없는 A＋B/공백 없는 A＋B.py
@@ -0,0 +1,11 @@
+import sys
+n = sys.stdin.readline().rstrip()
+if len(n) == 2:
+    print(int(n[0]) + int(n[1]))
+elif len(n) == 3:
+    if n[1] == "0":
+        print(int(n[:2]) + int(n[2]))
+    else:
+        print(int(n[0]) + int(n[1:]))
+elif len(n) == 4:
+    print(int(n[:2]) + int(n[2:]))
