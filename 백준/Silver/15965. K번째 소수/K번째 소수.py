aec35d38b7bf7741374d202162507173d283d72d
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2025-11-24 16:59:43 +0900

    [Silver II] Title: K번째 소수, Time: 2984 ms, Memory: 112664 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.py
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -1,13 +1,13 @@
 import sys
 import math
 n = int(sys.stdin.readline().rstrip())
-sosu = [0] * 500001
-for i in range(2, int(math.sqrt(500000) + 1)):
+sosu = [0] * 10000001
+for i in range(2, int(math.sqrt(10000000) + 1)):
     if sosu[i] == 0:
-        for j in range(i * i, 500000, i):
+        for j in range(i * i, 10000000, i):
             sosu[j] = 1
 cnt = 0
-for i in range(2, 500001):
+for i in range(2, 10000001):
     if sosu[i] == 0:
         cnt += 1
     if cnt == n:
