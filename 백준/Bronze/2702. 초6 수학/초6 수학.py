39ad5d54c1b4ecf129f318542ce3eb2176c740e8
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2025-12-13 18:00:01 +0900

    [Bronze II] Title: 초6 수학, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/2702. 초6 수학/초6 수학.py
@@ -0,0 +1,6 @@
+import sys
+import math
+n = int(sys.stdin.readline().rstrip())
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    print(math.lcm(a, b), math.gcd(a, b))
