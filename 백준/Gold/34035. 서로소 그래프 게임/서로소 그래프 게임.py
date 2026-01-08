c6e271d95c74937642162450986fee1944cb22b1
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-01-08 16:36:04 +0900

    [Gold III] Title: 서로소 그래프 게임, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/34035. 서로소 그래프 게임/서로소 그래프 게임.py
@@ -0,0 +1,8 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+for _ in range(n):
+    a = int(sys.stdin.readline().rstrip())
+    if a == 2:
+        print("kang")
+    else:
+        print("chan")
