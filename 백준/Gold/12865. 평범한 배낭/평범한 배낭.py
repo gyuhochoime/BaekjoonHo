9992b781fa1bf631d57b4f29b1be9152e949e545
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-02-02 16:27:20 +0900

    [Gold V] Title: 평범한 배낭, Time: 3456 ms, Memory: 283624 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,13 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+bp = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        weight = bp[i-1][0]
+        value = bp[i-1][1]
+        if j >= weight:
+            dp[i][j] = max(value + dp[i-1][j-weight], dp[i-1][j])
+        else:
+            dp[i][j] = dp[i-1][j]
+print(dp[n][m])
