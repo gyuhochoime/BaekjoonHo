6a8e1ae50fceff8e8f7d87abb80464f897b74bf4
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-02-03 20:06:51 +0900

    [Silver I] Title: 포도주 시식, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+grape = [int(sys.stdin.readline()) for _ in range(n)]
+dp = [0] * n
+dp[0] = grape[0]
+if n > 1:
+    dp[1] = grape[0] + grape[1]
+if n > 2:
+    dp[2] = max(dp[1], grape[0] + grape[2], grape[1] + grape[2])
+for i in range(3, n):
+    dp[i] = max(dp[i-1], dp[i-2] + grape[i], dp[i-3] + grape[i-1] + grape[i])
+print(dp[n-1])
