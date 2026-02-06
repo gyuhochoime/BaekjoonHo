ca6b127eb0c45dc16bb2e7a3d76e2cac5466ef8a
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-02-06 15:24:45 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 448 ms, Memory: 71968 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,9 @@
+import sys
+T = int(sys.stdin.readline())
+dp = [0] * 1000001
+dp[0], dp[1], dp[2] = 1, 2, 4
+for i in range(3, 1000001):
+    dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009
+for _ in range(T):
+    n = int(sys.stdin.readline())
+    print(dp[n-1])
