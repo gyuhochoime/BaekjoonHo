fe8275a3ae34e6b363930835023775a90b1996ac
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-02-08 23:03:13 +0900

    [Silver V] Title: 피보나치 수 4, Time: 52 ms, Memory: 36964 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+if n == 0:
+    print(0)
+elif n == 1:
+    print(1)
+else:
+    dp = [0] * (n + 1)
+    dp[0], dp[1] = 0, 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+    print(dp[n])
