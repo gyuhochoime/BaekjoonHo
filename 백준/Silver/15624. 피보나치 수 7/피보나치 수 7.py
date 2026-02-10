088fc50da2188d81dd1e26c34b25b5c7360541a9
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-02-10 23:21:26 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 332 ms, Memory: 71968 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -8,5 +8,5 @@ else:
     dp = [0] * (n + 1)
     dp[0], dp[1] = 0, 1
     for i in range(2, n + 1):
-        dp[i] = (dp[1] + dp[2]) % 1000000007
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007
     print(dp[n])
