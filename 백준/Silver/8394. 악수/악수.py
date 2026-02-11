c1820cb86ad96f2c8a9ed1307fac7af6bc30b41a
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-02-11 23:49:14 +0900

    [Silver III] Title: 악수, Time: 2188 ms, Memory: 110536 KB -BaekjoonHub

---
+++ 백준/Silver/8394. 악수/악수.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+else:
+    dp = [0] * (n + 1)
+    dp[1], dp[2] = 1, 2
+    for i in range(3, n + 1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 10
+    print(dp[n])
