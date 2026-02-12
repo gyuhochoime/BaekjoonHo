4725ea0b3f947f9d2c8922d4f8101ae14c21789a
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-02-12 15:55:28 +0900

    [Silver II] Title: 제곱수의 합, Time: 3496 ms, Memory: 33232 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+dp = [0] * (n + 1)
+sqrt = [x ** 2 for x in range(1, 318)]
+for i in range(1, n + 1):
+    mini = []
+    for j in sqrt:
+        if j > i:
+            break
+        mini.append(dp[i-j])
+    dp[i] = min(mini) + 1
+print(dp[-1])
