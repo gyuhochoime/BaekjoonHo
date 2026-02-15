cdd360cca35157057d1fe5090bb3c9aacd628d5b
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-02-15 22:18:49 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,13 +3,17 @@ n, m = map(int, sys.stdin.readline().split())
 country = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 country.sort(key = lambda x: (-x[1], -x[2], -x[3]))
 rank = 1
+tmp = 0
 prev_gold, prev_silver, prev_bronze = country[0][1], country[0][2], country[0][3]
 country_rank = dict()
-for num, gold, silver, bronze in country:
-    if prev_gold == gold and prev_silver == silver and prev_bronze == bronze:
-        country_rank[num] = rank
+country_rank[country[0][0]] = rank
+for i in range(1, n):
+    if prev_gold == country[i][1] and prev_silver == country[i][2] and prev_bronze == country[i][3]:
+        country_rank[country[i][0]] = rank
+        tmp += 1
     else:
-        rank += 1
-        country_rank[num] = rank
-    prev_gold, prev_silver, prev_bronze = gold, silver, bronze
+        rank += 1 + tmp
+        tmp = 0
+        country_rank[country[i][0]] = rank
+    prev_gold, prev_silver, prev_bronze = country[i][1], country[i][2], country[i][3]
 print(country_rank[m])
