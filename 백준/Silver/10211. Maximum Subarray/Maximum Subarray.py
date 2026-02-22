785d348104d09fb151f94b802cc928a29bb4d586
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-02-22 23:38:52 +0900

    [Silver IV] Title: Maximum Subarray, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.py
@@ -0,0 +1,10 @@
+import sys
+T = int(sys.stdin.readline())
+for _ in range(T):
+    n = int(sys.stdin.readline())
+    arr = list(map(int, sys.stdin.readline().split()))
+    dp = [0] * n
+    dp[0] = arr[0]
+    for i in range(1, n):
+        dp[i] = max(dp[i-1] + arr[i], arr[i])
+    print(max(dp))
