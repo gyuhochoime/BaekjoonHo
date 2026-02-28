40b5566b1d090cd9ee8131293db15b5ef840d63c
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-02-28 19:24:24 +0900

    [Silver IV] Title: Hexagonal Tiles, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/5705. Hexagonal Tiles/Hexagonal Tiles.py
@@ -0,0 +1,15 @@
+import sys
+while True:
+    n = int(sys.stdin.readline())
+    if n == 0:
+        break
+    if n == 1:
+        print(1)
+    elif n == 2:
+        print(2)
+    else:
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        print(dp[n])
