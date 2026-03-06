c918de4be85a1a5329476270540e58270fbff1bc
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-03-06 14:03:01 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 38156 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,16 +1,18 @@
 import sys
 from collections import deque
+def bfs(s):
+    q = deque()
+    q.append(s)
+    visited = [-1] * 100001
+    visited[s] = 0
+    while q:
+        a = q.popleft()
+        if a == m:
+            return visited[a]
+        for i in [a + 1, a - 1, 2 * a]:
+            if 0 <= i < 100001 and visited[i] == -1:
+                q.append(i)
+                visited[i] = visited[a] + 1
 n, m = map(int, sys.stdin.readline().split())
-arr = deque()
-arr.append(n)
-visited = [-1] * 100001
-visited[n] = 0
-while arr:
-    a = arr.popleft()
-    if a == m:
-        print(visited[a])
-        break
-    for i in [a - 1, a + 1, a * 2]:
-        if i >= 0 and i <= 100000 and visited[i] == -1:
-            visited[i] = visited[a] + 1
-            arr.append(i)
+ans = bfs(n)
+print(ans)
