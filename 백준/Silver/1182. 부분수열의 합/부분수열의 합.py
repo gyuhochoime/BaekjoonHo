2632d11daa27b2b4a55475c19caca99605780039
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-03-15 13:01:00 +0900

    [Silver II] Title: 부분수열의 합, Time: 224 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,15 +1,23 @@
 import sys
-def solve(a):
-    global cnt
-    if sum(tmp) == m and tmp:
-        cnt += 1
-    for i in range(a, n):
-        tmp.append(arr[i])
-        solve(i + 1)
-        tmp.pop()
-n, m = map(int, sys.stdin.readline().split())
-arr = list(map(int, sys.stdin.readline().split()))
+input = sys.stdin.readline
+
+n, s = map(int, input().split())
+num_li = list(map(int, input().split()))
+num_li.sort()
 cnt = 0
-tmp = []
-solve(0)
+
+def find(num, idx):
+    global cnt
+    if num == s:
+        cnt+=1
+    if idx == n:
+        return
+    
+    for i in range(idx, n):
+        find(num+num_li[i], i + 1)
+    
+for i in range(n):
+    find(num_li[i] ,i+1)
+    
 print(cnt)
+
