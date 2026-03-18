bfe973ae296a8ef11f96f5be198652c37ee2e111
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-03-18 14:59:49 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,13 +1,16 @@
 import sys
-def DFS(L, s):
-    if L == m:
-        for j in range(L):
-            print(res[j], end = " ")
-        print()
-    else:
-        for i in range(s, n + 1):
-            res[L] = i
-            DFS(L + 1, i)
+def dfs(a, arr):
+    if a == m:
+        ans.append(arr)
+        return
+    for i in range(1, n + 1):
+        if arr:
+            if i >= arr[-1]:
+                dfs(a + 1, arr + [i])
+        else:
+            dfs(a + 1, arr + [i])
 n, m = map(int, sys.stdin.readline().split())
-res = [0] * m
-DFS(0, 1)
+ans = []
+dfs(0, [])
+for i in ans:
+    print(*i)
