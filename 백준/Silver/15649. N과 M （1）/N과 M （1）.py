6845a88177ebb1addfedd8a422745ab792b93238
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-03-18 12:40:17 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 37528 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,17 +1,16 @@
 import sys
-def DFS(L):
-    if L == m:
-        for i in res:
-            print(i, end = " ")
-        print()
-    else:
-        for i in range(1, n + 1):
-            if ch[i] == 0:
-                ch[i] = 1
-                res[L] = i
-                DFS(L + 1)
-                ch[i] = 0
-n, m = map(int, sys.stdin.readline().rstrip().split())
-res = [0] * m
-ch = [0] * (n + 1)
-DFS(0)
+def dfs(a, arr):
+    if a == m:
+        ans.append(arr)
+        return
+    for i in range(1, n + 1):
+        if visited[i] == 0:
+            visited[i] = 1
+            dfs(a + 1, arr + [i])
+            visited[i] = 0
+n, m = map(int, sys.stdin.readline().split())
+ans = []
+visited = [0] * (n + 1)
+dfs(0, [])
+for i in ans:
+    print(*i)
