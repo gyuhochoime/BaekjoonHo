6777238750d59aff0a9a9ea63419d5ca422565f6
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-03-30 20:58:33 +0900

    [Gold IV] Title: 타일 채우기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -0,0 +1,9 @@
+import sys
+n = int(sys.stdin.readline())
+dp = [0] * 31
+dp[0], dp[2] = 1, 3
+for i in range(4, n + 1, 2):
+    dp[i] = dp[i-2] * 3
+    for j in range(i - 4, -1, -2):
+        dp[i] += dp[j] * 2
+print(dp[n])
