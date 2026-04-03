218074226904d253b0858798393031766fd36403
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-04-03 22:30:57 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 444 ms, Memory: 236632 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -0,0 +1,15 @@
+import sys
+arr_1 = list(sys.stdin.readline().rstrip())
+arr_2 = list(sys.stdin.readline().rstrip())
+n = len(arr_1)
+m = len(arr_2)
+ans = 0
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if arr_1[i-1] == arr_2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+            ans = max(ans, dp[i][j])
+        else:
+            dp[i][j] = 0
+print(ans)
