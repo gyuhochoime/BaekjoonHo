cd6bab6ea1d2b392da2e0af2e2a5e9b83dd5de88
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-04-06 19:08:53 +0900

    [Gold V] Title: 두 부분 문자열, Time: 444 ms, Memory: 40220 KB -BaekjoonHub

---
+++ 백준/Gold/11056. 두 부분 문자열/두 부분 문자열.py
@@ -0,0 +1,12 @@
+import sys
+arr_1 = list(sys.stdin.readline().rstrip())
+arr_2 = list(sys.stdin.readline().rstrip())
+n, m = len(arr_1), len(arr_2)
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if arr_1[i-1] == arr_2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+print(n + m - dp[n][m])
