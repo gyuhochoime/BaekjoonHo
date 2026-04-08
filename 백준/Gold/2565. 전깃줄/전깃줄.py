81a8c098a11ff5a1deceadaf0e86c368cb520ce3
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-04-08 10:24:58 +0900

    [Gold V] Title: 전깃줄, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,10 +1,10 @@
 import sys
 n = int(sys.stdin.readline())
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-arr.sort()
+pole = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+pole.sort()
 dp = [1] * n
 for i in range(1, n):
     for j in range(i):
-        if arr[j][1] < arr[i][1]:
+        if pole[i][1] > pole[j][1]:
             dp[i] = max(dp[i], dp[j] + 1)
 print(n - max(dp))
