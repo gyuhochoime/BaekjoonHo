63879a248f87af859228ff1f2bf00c2eb34bdf27
Gyuho Choi <87693903+gyuhochoime@users.noreply.github.com>
2026-04-13 20:10:26 +0900

    [Gold IV] Title: RGB거리 2, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -0,0 +1,15 @@
+import sys
+n = int(sys.stdin.readline())
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+tot = 10000000
+for i in range(3):
+    dp = [[10000000] * 3 for _ in range(n)]
+    dp[0][i] = arr[0][i]
+    for j in range(1, n):
+        dp[j][0] = min(dp[j-1][1], dp[j-1][2]) + arr[j][0]
+        dp[j][1] = min(dp[j-1][0], dp[j-1][2]) + arr[j][1]
+        dp[j][2] = min(dp[j-1][0], dp[j-1][1]) + arr[j][2]
+    for k in range(3):
+        if k != i:
+            tot = min(tot, dp[n-1][k])
+print(tot)
